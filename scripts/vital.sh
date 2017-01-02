d20a41476e89f5f3e083fccbd5cefd4626436e3a
kzm4269 <4269kzm@gmail.com>
2017-01-02 15:07:32 +0900

    update vital.sh

--- scripts/vital.sh
+++ scripts/vital.sh
@@ -2,26 +2,25 @@
 
 if test -z "${BASH_VERSION:-}" -a -z "${ZSH_VERSION:-}"; then
   echo "not supported: $SHELL" >&2
-  exit 1
+  return 1
 fi
 
 # check environment
 has() { type "$@" &>/dev/null; }
 is_login_shell() { [ "$SHLVL" = 1 ]; }
-is_interactive_shell() { [ "${-/i/}" != "$-" ]; }
+is_interactive_shell() { [[ "$-" = *i* ]]; }
 is_bash() { [ -n "${BASH_VERSION:-}" ]; }
 is_zsh() { [ -n "${ZSH_VERSION:-}" ]; }
 is_ssh() { [ -n "${SSH_CONNECTION:-}" ]; }
 is_screen() { [ -n "${STY:-}" ]; }
 is_tmux() { [ -n "${TMUX:-}" ]; }
-is_linux() { uname | grep -q 'Linux'; }
-is_darwin() { uname | grep -q 'Darwin'; }
-is_ubuntu() { uname -v | grep -q 'Ubuntu'; }
+is_linux() { [[ "$(uname)" = Linux ]]; }
+is_darwin() { [[ "$(uname)" = Darwin ]]; }
 is_git_repo() { git rev-parse --is-inside-work-tree &>/dev/null; }
 
 # string
 strlen() { echo ${#1}; }
-strstr() { [ "${1#*"$2"}" != "$1" ]; }
+strstr() { [[ "$1" = *"$2"* ]]; }
 isdigit() { [ "$1" -eq "$1" ] &>/dev/null; }
 lower() { 
   [ $# -gt 1 ] && return 1
@@ -35,14 +34,14 @@ upper() {
 # alias
 _alias_stack=()
 alias_push() {
-  [ -z "${BASH_VERSION}" ] || _alias_stack=(${_alias_stack[@]} "$(alias -p)")
-  [ -z "${ZSH_VERSION}" ] || _alias_stack=(${_alias_stack[@]} "$(alias -L)")
+  is_bash && _alias_stack=(${_alias_stack[@]} "$(alias -p)")
+  is_zsh && _alias_stack=(${_alias_stack[@]} "$(alias -L)")
   unalias -a
 }
 alias_pop() {
-  [ -z "${ZSH_VERSION}" ] || setopt localoptions ksharrays
+  is_zsh && setopt localoptions ksharrays
   eval "${_alias_stack[0]}"
-  [ -z "${BASH_VERSION}" ] || _alias_stack=${_alias_stack[@]:1}
-  [ -z "${ZSH_VERSION}" ] || _alias_stack=${_alias_stack[@]:1}
+  is_bash && _alias_stack=${_alias_stack[@]:1}
+  is_zsh && _alias_stack=${_alias_stack[@]:1}
 }
 
