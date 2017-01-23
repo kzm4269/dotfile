8a1377baa3f8b5fd9dd4149e9a006bd3f307c19e
kzm4269 <4269kzm@gmail.com>
2017-01-23 19:29:14 +0900

    add xterm-color-count

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -1,6 +1,10 @@
 zplug 'zsh-users/zsh-completions'
 zplug 'zsh-users/zsh-history-substring-search'
 zplug 'zsh-users/zsh-syntax-highlighting'
+zplug 'l0b0/xterm-color-count', \
+  as:command, \
+  use:xterm-color-count.sh, \
+  rename-to:xterm-color-count
 
 [ -s "$HOME/local/etc/zsh/zplug.zsh" ] &&
   source "$HOME/local/etc/zsh/zplug.zsh"
