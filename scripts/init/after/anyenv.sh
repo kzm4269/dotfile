cd5836381c1f4315e6b55191d44a30daa371dae8
kzm4269 <4269kzm@gmail.com>
2017-01-23 13:51:36 +0900

    update

--- scripts/init/after/anyenv.sh
+++ scripts/init/after/anyenv.sh
@@ -4,12 +4,14 @@ anyenv_home="$HOME/.anyenv"
 anyenv="$anyenv_home/bin/anyenv"
 
 # anyenv
-[ -d "$anyenv_home" ] || git clone https://github.com/riywo/anyenv "$anyenv_home"
+[ -d "$anyenv_home" ] ||
+  git clone https://github.com/riywo/anyenv "$anyenv_home"
 eval "$("$anyenv" init -)"
 
 # pyenv
-[ -d "$anyenv_home/envs/pyenv" ] || "$anyenv" install pyenv
+[ -d "$anyenv_home/envs/pyenv" ] ||
+  "$anyenv" install pyenv
 
 # pyenv-virtualenv
 [ -d "$anyenv_home/envs/pyenv/plugins/pyenv-virtualenv" ] ||
-	git clone https://github.com/yyuu/pyenv-virtualenv.git "$anyenv_home/envs/pyenv/plugins/pyenv-virtualenv"
+	git clone https://github.com/yyuu/pyenv-virtualenv "$anyenv_home/envs/pyenv/plugins/pyenv-virtualenv"
