e860a0953ce91902780147c659dcf0dd12898ac9
kzm4269 <4269kzm@gmail.com>
2017-01-28 08:57:16 +0900

    speed up

--- .zsh/rc/99_etc.zsh
+++ .zsh/rc/99_etc.zsh
@@ -5,5 +5,15 @@
   source ~/.autojump/share/autojump/autojump.zsh
 
 if has pip; then
-  eval "$(pip completion --zsh)"
+  # eval "$(pip completion --zsh)"  # too slow
+  
+  function _pip_completion {
+    local words cword
+    read -Ac words
+    read -cn cword
+    reply=( $( COMP_WORDS="$words[*]" \
+      COMP_CWORD=$(( cword-1 )) \
+      PIP_AUTO_COMPLETE=1 $words[1] ) )
+  }
+  compctl -K _pip_completion pip
 fi
