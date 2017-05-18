2df086a04254fd9149274cfaa973dfdc27bdce9c
kzm4269 <4269kzm@gmail.com>
2017-05-18 01:03:08 +0000

    Fix zplug install script

--- scripts/init/after/zplug.sh
+++ scripts/init/after/zplug.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eu
+export ZPLUG_HOME="$HOME/.zplug"
 
-if [ ! -d "$HOME/.zplug" ]; then
-  curl -sL zplug.sh/installer | zsh; 
+if [ ! -d "$ZPLUG_HOME" ]; then
+  git clone https://github.com/zplug/zplug "$ZPLUG_HOME"
 fi
+zsh "$ZPLUG_HOME/init.zsh"
