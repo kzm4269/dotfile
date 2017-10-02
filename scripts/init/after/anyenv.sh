c4d9bda0df2e09f68eb85e4a58574b5ea158cc0f
kzm4269 <4269kzm@gmail.com>
2017-10-02 11:04:53 +0900

    add pyenv-which-ext

--- scripts/init/after/anyenv.sh
+++ scripts/init/after/anyenv.sh
@@ -2,6 +2,7 @@
 set -eu
 anyenv_home="$HOME/.anyenv"
 anyenv="$anyenv_home/bin/anyenv"
+pyenv_home="$anyenv_home/envs/pyenv"
 
 # anyenv
 [ -d "$anyenv_home" ] ||
@@ -9,9 +10,14 @@ anyenv="$anyenv_home/bin/anyenv"
 eval "$("$anyenv" init -)"
 
 # pyenv
-[ -d "$anyenv_home/envs/pyenv" ] ||
+[ -d "$pyenv_home" ] ||
   "$anyenv" install pyenv
 
 # pyenv-virtualenv
-[ -d "$anyenv_home/envs/pyenv/plugins/pyenv-virtualenv" ] ||
-  git clone https://github.com/yyuu/pyenv-virtualenv "$anyenv_home/envs/pyenv/plugins/pyenv-virtualenv"
+[ -d "$pyenv_home/plugins/pyenv-virtualenv" ] ||
+  git clone https://github.com/yyuu/pyenv-virtualenv "$pyenv_home/plugins/"
+
+# pyenv-which-ext
+[ -d "$pyenv_home/plugins/pyenv-virtualenv" ] ||
+  git clone https://github.com/pyenv/pyenv-which-ext "$pyenv_home/plugins/"
+
