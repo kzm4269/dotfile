c3e2bc3bf3d8f21d8c43e473417b2c7bbf9dd1ad
kzm4269 <4269kzm@gmail.com>
2017-10-31 05:16:10 +0900

    update zshrc

--- .zsh/after/rc/00_alias.zsh
+++ .zsh/after/rc/00_alias.zsh
@@ -1,13 +1,14 @@
 # -- basic
 alias sudo='sudo '
 
-alias la='ls -A'
-alias ll='ls -lh'
-alias lla='ls -lhA'
+alias ls='ls -hF'
+alias la='ls -a'
+alias ll='ls -l'
+alias lla='ls -la'
 
-alias du="du -h"
-alias df="df -h"
-alias su="su -l"
+alias du='du -h'
+alias df='df -h'
+alias su='su -l'
 
 alias jobs='jobs -l'
 
@@ -20,31 +21,26 @@ has 'colordiff' &&
   alias diff='colordiff -u' ||
   alias diff='diff -u'
 
-is_darwin && alias ls='ls -G -w -F'
-is_linux && alias ls='ls --color=auto -F'
+case "$OSTYPE" in
+  freebsd*|darwin*) alias ls='ls -G -w';;
+  linux*) alias ls='ls --color=auto';;
+esac
 
 # -- zmv
 autoload -Uz zmv
 alias zmvw='noglob zmv -W'
 
 # -- single character
-alias G="grep"
-alias L="less"
-alias S="sort"
-alias T="tail"
-alias H="head"
-alias X="xargs -I{}"
+alias G='grep'
+alias L='less'
+alias S='sort'
+alias T='tail'
+alias H='head'
+alias X='xargs -I{}'
 
-has fzf && alias F="fzf"
+has fzf && alias F='fzf'
 
 case "$OSTYPE" in
-  freebsd*|darwin*) alias O="open";;
-  linux*) alias O="xdg-open";;
+  freebsd*|darwin*) alias O='open';;
+  linux*) alias O='xdg-open';;
 esac
-
-# -- etc
-has trash-put && alias tp='trash-put'
-
-if has keychain; then
-  alias eval_keychain=''
-fi
