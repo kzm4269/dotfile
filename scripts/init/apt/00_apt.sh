8ac6d79c6f84f8bfab600788afd201a657f9159f
Kazuma Takahara <4269kzm@gmail.com>
2018-02-09 08:00:28 +0900

    ちょっと整理

--- scripts/init/apt/00_apt.sh
+++ scripts/init/apt/00_apt.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo apt-get update || exit 1
-sudo apt-get install -y aptitude
 sudo apt-get install -y build-essential
 sudo apt-get install -y cmake
 sudo apt-get install -y curl
@@ -15,7 +14,6 @@ sudo apt-get install -y libncurses5-dev
 sudo apt-get install -y p7zip
 sudo apt-get install -y subversion
 sudo apt-get install -y tmux
-sudo apt-get install -y trash-cli
 sudo apt-get install -y unar
 sudo apt-get install -y vim-gnome
 sudo apt-get install -y xsel
