a433162bb3bcf85cf07ec03d0b47f0a650177db0
Kazuma Takahara <4269kzm@gmail.com>
2018-07-17 21:38:23 +0900

    Skip ls if there are too many files

--- .zsh/after/rc/99_etc.zsh
+++ .zsh/after/rc/99_etc.zsh
@@ -1,4 +1,10 @@
-function auto_ls() { ls; }
+function auto_ls() { 
+  if has tput && (( $(ls --format=horizontal | wc -l) < $(tput lines) - 3 )); then
+    ls
+  else
+    echo "... $(ls | wc -l) files ..." 
+  fi
+}
 chpwd_functions=($chpwd_functions auto_ls)
 
 [ -s ~/.gibo/gibo-completion.zsh ] &&
