8e476f3c0f8253225ec877af0838fccc39dbdfc0
Kazuma Takahara <4269kzm@gmail.com>
2018-07-21 17:59:46 +0900

    Support direnv for armv7l

--- scripts/init/after/direnv.sh
+++ scripts/init/after/direnv.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 set -eu
+latest=$(curl https://github.com/direnv/direnv/releases/latest | perl -lne 'print $1 if m!/(v[0-9.]+)"!')
 direnv_home="$HOME/.direnv"
 
-[ -d "$direnv_home" ] || git clone https://github.com/direnv/direnv "$direnv_home"
-cd "$direnv_home"; make
+case "$(uname -sm)" in
+  'Linux armv7l')
+    suffix='linux-arm'
+    ;;
+  *) 
+    echo 'unknown machine: '$(uname -sm) >&2
+    exit 1
+esac
+
+direnv_basename="direnv.$latest.$suffix"
+if [[ -e "$direnv_home/$direnv_basename" ]]; then
+  exit
+fi
+
+mkdir -p "$direnv_home"
+wget https://github.com/direnv/direnv/releases/download/$latest/direnv.$suffix -O "$direnv_home/$direnv_basename"
+chmod u+x "$direnv_home/$direnv_basename"
+ln -snf "$direnv_basename" "$direnv_home/direnv"
--- scripts/update/repos.sh
+++ scripts/update/repos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-repos=(~/.fzf ~/.direnv ~/.anyenv ~/.anyenv/envs/pyenv ~/.zplug)
+repos=(~/.fzf ~/.anyenv ~/.anyenv/envs/pyenv ~/.zplug)
 for repo in "${repos[@]}"; do
   if [ -d "$repo" ]; then
     echo "update: $repo"
@@ -9,4 +9,8 @@ done
 
 if [ -d ~/.fzf ]; then
   ~/.fzf/install --key-bindings --completion --no-update-rc
+fi
+
+if [ -d ~/.direnv ]; then
+  bash "$DOTPATH/scripts/init/after/direnv.sh"
 fi
