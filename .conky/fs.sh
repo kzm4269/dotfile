30cd615159838e09edb8c95a6f46702f6032dd4a
Kazuma Takahara <4269kzm@gmail.com>
2018-12-18 10:03:07 +0900

    Update .conky
    マウントポイントのパスの省略方法を変更

--- .conky/fs.sh
+++ .conky/fs.sh
@@ -8,7 +8,7 @@ _fs_info() {
   [[ $dev =~ ^/dev/loop[0-9]*$ ]] && return 0
   
   fs_short="$fs"
-  [[ ${#fs_short} -gt 15 ]] && fs_short="${fs_short##*/}"
+  [[ ${#fs_short} -gt 15 ]] && fs_short="..${fs_short##*/}"
   
   echo "$fs_short"'${alignr}${fs_used '"$fs"'}/${fs_size '"$fs"'}'
   echo '${fs_bar 6,200 '"$fs"'}${alignr}${fs_used_perc '"$fs"'}%'
