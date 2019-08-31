f5811e7e67690f1f1defeaf90c84d048b242fd12
Kazuma Takahara <4269kzm@gmail.com>
2019-08-31 20:43:46 +0900

    Fix a bug

--- scripts/init/after/autojump.sh
+++ scripts/init/after/autojump.sh
@@ -3,7 +3,7 @@ set -eu
 
 if [[ ! -d "$HOME/.autojump" ]]; then
   tmp=$(mktemp -d)
-  git clone git://github.com/joelthelion/autojump.git $tmp/autojump
+  git clone https://github.com/joelthelion/autojump $tmp/autojump
   cd $tmp/autojump
   ./install.py
   rm -rf $tmp
