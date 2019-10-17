60ff114801ef25808cd0e874c2a13822faccdaa4
Kazuma Takahara <4269kzm@gmail.com>
2019-10-17 19:51:35 +0900

    refactor 00_apt.sh

--- scripts/init/apt/00_apt.sh
+++ scripts/init/apt/00_apt.sh
@@ -12,21 +12,21 @@ else
 fi
 
 sudo $apt update || exit 1
+
+#  build
 sudo $apt install -y build-essential
-sudo $apt install -y cmake
-sudo $apt install -y curl
-sudo $apt install -y fcitx-mozc
-sudo $apt install -y ffmpeg
-sudo $apt install -y gimp
-sudo $apt install -y git
-sudo $apt install -y golang
-sudo $apt install -y imagemagick
-sudo $apt install -y inkscape
 sudo $apt install -y libncurses5-dev
-sudo $apt install -y p7zip
-sudo $apt install -y subversion
-sudo $apt install -y tmux
+sudo $apt install -y cmake
+
+# network
+sudo $apt install -y curl
+
+# archive
 sudo $apt install -y unar
-sudo $apt install -y vim-gnome
-sudo $apt install -y xsel
+
+# terminal
+sudo $apt install -y tmux
+sudo $apt install -y vim-nox
 sudo $apt install -y zsh
+sudo $apt install -y golang
+sudo $apt install -y xsel
