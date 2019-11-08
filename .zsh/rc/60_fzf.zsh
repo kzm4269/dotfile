5866a7373b199c148abe1df71f9b080b3541a812
Kazuma Takahara <4269kzm@gmail.com>
2019-11-08 11:42:08 +0900

    Use ripgrep/bat

--- .zsh/rc/60_fzf.zsh
+++ .zsh/rc/60_fzf.zsh
@@ -29,3 +29,6 @@ if [ "$(tput colors)" -ge 256 ]; then
 fi
 
 export FZF_COMPLETION_TRIGGER='~~'
+
+export FZF_CTRL_T_COMMAND='rg --files --hidden --follow --glob "!.git/*" 2>/dev/null'
+export FZF_CTRL_T_OPTS='--preview "bat  --color=always --style=header,grid --line-range :100 {}"'
