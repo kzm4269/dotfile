de29760f99867cf8e94c4c3dd620637ba7979727
Kazuma Takahara <4269kzm@gmail.com>
2020-07-20 11:00:22 +0900

    Add installation nvim

--- scripts/init/after/vim.sh
+++ scripts/init/after/vim.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -eu
 
+if ! type nvim &>/dev/null; then
+  curl -L https://github.com/neovim/neovim/releases/download/stable/nvim.appimage > ~/local/bin/nvim
+  chmod u+x ~/local/bin/nvim
+fi
+
 cd ~/.vim
 make all
